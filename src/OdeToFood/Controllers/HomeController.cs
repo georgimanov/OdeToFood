e970b08c210489226b4ac821ed0c0c828b9721a2
Georgi Manov <georgimanov@gmail.com>
2016-12-04 14:47:37 +0200

    added HomePageViewModel

--- src/OdeToFood/Controllers/HomeController.cs
+++ src/OdeToFood/Controllers/HomeController.cs
@@ -1,21 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
-        IRestaurantData _restaurantData;
+        private IGreeter _greeter;
+        private IRestaurantData _restaurantData;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(
+            IRestaurantData restaurantData, 
+            IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public ViewResult Index()
         {
-            var model = _restaurantData.GetAll();
+            var model = new HomePageViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentGreeting = _greeter.GetGreeting();
 
             return View(model);
         }
--- src/OdeToFood/Services/RestaurantData.cs
+++ src/OdeToFood/Services/RestaurantData.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-using OdeToFood.Models;
+using OdeToFood.Entities;
 
 namespace OdeToFood.Services
 {
