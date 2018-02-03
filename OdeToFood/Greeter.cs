6256bee48209f5f9fb01c677d58a80b0a6f389f9
Georgi Manov <georgimanov@gmail.com>
2018-02-03 22:52:45 +0200

    added IGreeter service

---
+++ OdeToFood/Greeter.cs
@@ -0,0 +1,19 @@
+﻿namespace OdeToFood
+{
+    using Microsoft.Extensions.Configuration;
+
+    internal class Greeter : IGreeter
+    {
+        private readonly IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetGreeting()
+        {
+            return _configuration["Greeting"];
+        }
+    }
+}
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -11,14 +11,16 @@
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
-        {
-        }
+		{
+		    services.AddSingleton<IGreeter, Greeter>();
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(
             IApplicationBuilder app,
             IHostingEnvironment env,
-            IConfiguration configuration)
+            IConfiguration configuration,
+            IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +29,8 @@
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = greeter.GetGreeting();
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
