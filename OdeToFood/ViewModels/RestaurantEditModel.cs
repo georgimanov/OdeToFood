1d0c3355d298a3e05766a1d00c10e6b5b1e3e1d7
Georgi Manov <georgimanov@gmail.com>
2018-02-04 15:52:36 +0200

    added RestaurantEditModel

---
+++ OdeToFood/ViewModels/RestaurantEditModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OdeToFood.ViewModels
+{
+    using OdeToFood.Models;
+
+    public class RestaurantEditModel
+    {
+        public string Name { get; set; }
+
+        public CuisineType Cuisine { get; set; }
+    }
+}
