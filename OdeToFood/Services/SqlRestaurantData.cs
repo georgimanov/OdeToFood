483d09a0fd2d3bb7eeefec004241a261a510fb39
Georgi Manov <georgimanov@gmail.com>
2018-02-04 17:15:32 +0200

    added SqlRestaurantData implementation

---
+++ OdeToFood/Services/SqlRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿namespace OdeToFood.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using OdeToFood.Data;
+    using OdeToFood.Models;
+
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants.OrderBy(r => r.Name);
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _context.Restaurants.Add(restaurant);
+            _context.SaveChanges();
+
+            return restaurant;
+        }
+    }
+}
