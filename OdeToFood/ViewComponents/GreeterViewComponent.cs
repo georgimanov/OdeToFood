d96146191eaa711cfab70e9855bedca5427909c5
Georgi Manov <georgimanov@gmail.com>
2018-02-04 23:52:50 +0200

    added view component

---
+++ OdeToFood/ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using OdeToFood.Services;
+
+namespace OdeToFood.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class GreeterViewComponent : ViewComponent
+    {
+        private readonly IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetGreeting();
+
+            return View("Default", model);
+        }
+    }
+}
