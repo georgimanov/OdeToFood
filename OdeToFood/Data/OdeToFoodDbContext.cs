c7f79297bf8800bb1a56112d89643fb793d74049
Georgi Manov <georgimanov@gmail.com>
2018-02-04 17:15:13 +0200

    added OdeToFood dbContext

---
+++ OdeToFood/Data/OdeToFoodDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace OdeToFood.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using OdeToFood.Models;
+
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
