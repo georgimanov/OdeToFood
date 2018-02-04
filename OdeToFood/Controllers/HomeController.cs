cccc6e3c0bd03587861c6fc431e399510a051daf
Georgi Manov <georgimanov@gmail.com>
2018-02-04 09:21:04 +0200

    added home controller

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿namespace OdeToFood.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return $"Hi, from {nameof(HomeController)}";
+        }
+    }
+}
