7d66942b856e91f63362675f4e1618b68c13f1fc
Georgi Manov <georgimanov@gmail.com>
2018-02-06 17:44:29 +0200

    Added LoggingMiddleware.cs

---
+++ OdeToFood/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,21 @@
+﻿namespace OdeToFood.Middleware
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+
+    public class LoggingMiddleware
+    {
+        private RequestDelegate _nextDelegate;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            this._nextDelegate = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await this._nextDelegate.Invoke(context);
+        }
+    }
+}
